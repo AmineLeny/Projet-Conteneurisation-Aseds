c75114add8c912630bc355fda5c8210e77317caa
AmineLeny <72925780+AmineLeny@users.noreply.github.com>
2024-12-15 04:10:23 +0100

    Make necessary change to front and back to deploy
    I've changed web config on backend to allow the
    front container to access endpoints after
    running containers in the network
    I've fixed some issues concerning image building
    for frontend and applied some good practices
    I've fixed nginx server config, and priveliges
    issues

--- Backend/StudentApp/src/main/java/aseds/ine2/Etudiant/StudentApp/WebConfig.java
+++ Backend/StudentApp/src/main/java/aseds/ine2/Etudiant/StudentApp/WebConfig.java
@@ -10,9 +10,9 @@ public class WebConfig implements WebMvcConfigurer {
 
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        // Allow requests from the frontend (React) URL
+        // Allow requests from the frontend (React) container
         registry.addMapping("/**") // Allow all paths
-                .allowedOrigins("http://localhost:5173") // Frontend URL
+                .allowedOrigins("http://localhost:5173", "http://frontend-container:5173")
                 .allowedMethods("GET", "POST", "PUT", "DELETE") // Allowed HTTP methods
                 .allowCredentials(true); // Allow cookies or authentication headers
     }
